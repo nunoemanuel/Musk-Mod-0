3b9d5c051041d3c79ef18ef5a48ac0dcd3d37522
Nuno Emanuel <npintomari1@educacion.navarra.es>
2026-04-27 22:41:45 +0200

    Actividad0.6.6

--- Actividad0.6.6.js
+++ Actividad0.6.6.js
@@ -1,29 +1,10 @@
 const readLine = require("readline-sync");
 
-// Función reutilizable
-function esBisiesto(año) {
-    return (año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0);
-}
+const año = readLine.question("Introduce un año: ");
 
-// Bucle principal
-while (true) {
-    const input = readLine.question("Introduce un año (o escribe 'salir'): ");
+if ((año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0)) {
+      console.log("El año introducido es bisiesto.");
 
-    if (input.toLowerCase() === "salir") {
-        console.log("Programa Finalizado.");
-        break;
-    }
-
-    const año = Number(input);
-
-    if (!Number.isInteger(año)) {
-        console.log("Error: introduce un número entero válido.\n");
-        continue;
-    }
-
-    console.log(
-        esBisiesto(año)
-            ? `El año ${año} es bisiesto.\n`
-            : `El año ${año} no es bisiesto.\n`
-    );
-}
+ } else {
+      console.log("El año introducido no es bisiesto.");
+ }
